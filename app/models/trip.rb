35080e9271e205a05a840dc46e5ff3c4e61314c0
AJ Harmond <ajharmond@gmail.com>
2019-03-09 10:48:33 -0700

    Bug fixes

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,11 +1,4 @@
 class Trip < ApplicationRecord
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-=======
   belongs_to :user
->>>>>>> Stashed changes
   has_many :locations
-=======
-    has_many :locations
->>>>>>> Stashed changes
 end
