e9f1d7a56cb6d9065421a5939a3d15dcede05536
hgarbe <h.garbe@live.com>
2019-03-09 10:38:51 -0700

    devise

--- db/schema.rb
+++ db/schema.rb
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(version: 2019_03_09_171919) do
     t.string "name"
     t.datetime "start_date"
     t.datetime "end_date"
+    t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id"
