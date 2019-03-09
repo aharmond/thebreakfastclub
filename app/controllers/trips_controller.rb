8639c651c09bdfd42617056da07643115927f880
Kaleb Allen <7kaleb29allen@gmail.com>
2019-03-09 10:32:09 -0700

    trip controller

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,13 +1,48 @@
 class TripsController < ApplicationController
+  before_action :set_trip, only: [:show, :edit, :update, :destroy]
   def index
+    @trips = Trip.all
   end
 
   def show
   end
 
   def new
+    @trip = Trip.new
+  end
+
+  def create
+    @trip = Trip.new(trip_params)
+    if @trip.save 
+      redirect_to trips_path
+    else 
+      render :new
+    end
   end
 
   def edit
   end
+
+  def update
+    if @trip.update(trip_params)
+      redirect_to trips_path
+    else 
+      render :edit
+    end
+  end
+
+  def destroy
+    @trip.destroy(trip_params)
+    redirect_to trips_path
+  end
+
+  private
+    def set_trip
+      @trip = Trip.single_trip(current_user.id, params[:id])
+    end
+
+    def trip_params
+      params.require(:trip).permit(:name, :start_date, :end_date)
+    end
+
 end
--- app/models/location.rb
+++ app/models/location.rb
@@ -1,4 +1,8 @@
 class Location < ApplicationRecord
+<<<<<<< Updated upstream
   belongs_to :trip
   has_one :address
+=======
+    has_one :address
+>>>>>>> Stashed changes
 end
